2b00c6a5b123e8813cef38a2ab5371306ebf41fc
Bohdan_Asieiev <aseevbogdan5@gmail.com>
2024-01-08 10:45:30 +0200

    feat: add RangeSumOfBST solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/RangeSumOfBST.kt
@@ -0,0 +1,35 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Refrence: https://leetcode.com/problems/range-sum-of-bst
+class RangeSumOfBST {
+    private var result = 0
+    private var low = 0
+    private var high = 0
+    fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+        this.low = low
+        this.high = high
+        iterateLeaves(root)
+        return result
+    }
+
+    private fun iterateLeaves(node: TreeNode?) {
+        if (node != null) {
+            if (node.`val` in low..high) {
+                result += node.`val`
+            }
+            if (node.`val` > low) {
+                iterateLeaves(node.left)
+            }
+            if (node.`val` < high) {
+                iterateLeaves(node.right)
+            }
+        }
+    }
+
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
