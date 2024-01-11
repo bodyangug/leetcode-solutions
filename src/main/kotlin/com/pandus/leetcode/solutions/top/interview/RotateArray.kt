360dea77addd96c3e7058b6c80cc1c10885df6b0
Bohdan_Asieiev <aseevbogdan5@gmail.com>
2024-01-11 20:12:53 +0200

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/top/interview/RotateArray.kt
@@ -0,0 +1,30 @@
+package com.pandus.leetcode.solutions.top.interview
+
+class RotateArray {
+    fun rotate1(nums: IntArray, k: Int): Unit {
+        nums.mapIndexed { index, i ->
+            Pair((index + k) % nums.size, i)
+        }.forEach {
+            nums[it.first] = it.second
+        }
+    }
+
+    fun rotate2(nums: IntArray, k: Int): Unit {
+        val steps = k % nums.size
+        nums.reverse()
+        nums.reverse(0, steps)
+        nums.reverse(steps, nums.size)
+    }
+
+    private fun IntArray.reverse(from: Int, to: Int) {
+        var start = from
+        var end = to - 1
+        while (start < end) {
+            val temp = this[start]
+            this[start] = this[end]
+            this[end] = temp
+            start++
+            end--
+        }
+    }
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/weekly/LeftmostColumnWithAtLeastOne.kt
@@ -0,0 +1,38 @@
+package com.pandus.leetcode.solutions.weekly
+
+import kotlin.math.min
+
+class LeftmostColumnWithAtLeastOne {
+    fun leftMostColumnWithOne(binaryMatrix: BinaryMatrix): Int {
+        val rows = binaryMatrix.dimensions()[0]
+        val cols = binaryMatrix.dimensions()[1]
+        var smallestIndex = cols
+        for (row in 0..<rows) {
+            var lo = 0
+            var hi = cols - 1
+            while (lo < hi) {
+                val mid = (lo + hi) / 2
+                if (binaryMatrix.get(row, mid) == 0) {
+                    lo = mid + 1
+                } else {
+                    hi = mid
+                }
+            }
+
+            if (binaryMatrix.get(row, lo) == 1) {
+                smallestIndex = min(smallestIndex, lo)
+            }
+        }
+        return if (smallestIndex == cols) -1 else smallestIndex
+    }
+}
+
+class BinaryMatrix {
+    fun get(row: Int, col: Int): Int {
+        return 0
+    }
+
+    fun dimensions(): List<Int> {
+        return emptyList()
+    }
+}
