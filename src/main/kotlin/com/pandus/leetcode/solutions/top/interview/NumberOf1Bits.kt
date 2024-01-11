120ce799ae5f7398ceda395148047729b990f2b2
Bohdan_Asieiev <aseevbogdan5@gmail.com>
2024-01-11 21:34:00 +0200

    chore: add solutions for bit manipulation tasks

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/top/interview/NumberOf1Bits.kt
@@ -0,0 +1,8 @@
+package com.pandus.leetcode.solutions.top.interview
+
+//Reference: https://leetcode.com/problems/number-of-1-bits
+class NumberOf1Bits {
+    fun hammingWeight(n: Int): Int {
+        return Integer.bitCount(n)
+    }
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/top/interview/ReverseBits.kt
@@ -0,0 +1,14 @@
+package com.pandus.leetcode.solutions.top.interview
+
+
+//Reference: https://leetcode.com/problems/reverse-bits
+class ReverseBits {
+    fun reverseBits(n: Int): Int {
+        var ans = 0
+        for (i in 0..31) {
+            ans = ans.shl(1)
+            ans = ans or (n.shr(i) and 1)
+        }
+        return ans
+    }
+}
