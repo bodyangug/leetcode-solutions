5386678648d75433a03e59e45f8f3a30fe0c89b6
bodyangug <aseevbogdan5@gmail.com>
2024-01-17 10:35:36 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/UniqueNumberOfOccurrences.kt
@@ -0,0 +1,14 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/unique-number-of-occurrences
+class UniqueNumberOfOccurrences {
+    fun uniqueOccurrences(arr: IntArray): Boolean {
+        return arr.groupBy { it }
+            .map { it.value.size }
+            .let { it.size == it.toSet().size }
+    }
+}
+
+fun main() {
+    UniqueNumberOfOccurrences().uniqueOccurrences(intArrayOf(1, 2))
+}
