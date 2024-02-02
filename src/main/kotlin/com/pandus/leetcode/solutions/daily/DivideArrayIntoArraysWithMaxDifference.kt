9425d07bad31691e487de0e5ad3459502bd2f359
Bohdan <bohdan_asieiev@epam.com>
2024-02-02 13:58:10 +0200

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/DivideArrayIntoArraysWithMaxDifference.kt
@@ -0,0 +1,15 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference:https://leetcode.com/problems/divide-array-into-arrays-with-max-difference
+class DivideArrayIntoArraysWithMaxDifference {
+    fun divideArray(nums: IntArray, k: Int): Array<IntArray> {
+        nums.sort()
+        val chunks = nums
+            .toList()
+            .chunked(3)
+        if (chunks.any { chunk -> chunk.maxOrNull()!! - chunk.minOrNull()!! > k }) {
+            return emptyArray()
+        }
+        return chunks.map { it.toIntArray() }.toTypedArray()
+    }
+}
