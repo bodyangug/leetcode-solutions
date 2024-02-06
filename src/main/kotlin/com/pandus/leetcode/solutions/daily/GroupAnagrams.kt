cef385b1ebe4e9cdfcaf3f4ab14512c086e00a6b
Bohdan <bohdan_asieiev@epam.com>
2024-02-06 13:40:01 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/GroupAnagrams.kt
@@ -0,0 +1,15 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/group-anagrams
+class GroupAnagrams {
+    fun groupAnagrams(strs: Array<String>): List<List<String>> {
+        val ans = mutableMapOf<String, MutableList<String>>()
+        strs.forEach {
+            val sortedKey = it.toList().sorted().toString()
+            ans.computeIfAbsent(sortedKey) { mutableListOf() }.add(it)
+        }
+        return ans.values.toList()
+    }
+
+
+}
