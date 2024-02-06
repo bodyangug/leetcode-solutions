edc61a5130a3d751079f399294f8ded40a4b0f7e
bodyangug <aseevbogdan5@gmail.com>
2024-02-06 18:06:37 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/PartitionArrayForMaximumSum.kt
@@ -0,0 +1,24 @@
+package com.pandus.leetcode.solutions.daily
+
+import java.util.*
+import kotlin.math.max
+import kotlin.math.min
+
+//Reference: https://leetcode.com/problems/partition-array-for-maximum-sum
+class PartitionArrayForMaximumSum {
+    fun maxSumAfterPartitioning(arr: IntArray, k: Int): Int {
+        val N = arr.size
+        val dp = IntArray(N + 1)
+        Arrays.fill(dp, 0)
+        for (start in N - 1 downTo 0) {
+            var currMax = 0
+            val end = min(N.toDouble(), (start + k).toDouble()).toInt()
+            for (i in start until end) {
+                currMax = max(currMax.toDouble(), arr[i].toDouble()).toInt()
+                dp[start] =
+                    max(dp[start].toDouble(), (dp[i + 1] + currMax * (i - start + 1)).toDouble()).toInt()
+            }
+        }
+        return dp[0]
+    }
+}
