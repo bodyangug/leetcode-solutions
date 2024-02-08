4fff9b23cd79c06a386ba06e80508270635e51da
Bohdan <bohdan_asieiev@epam.com>
2024-02-08 12:32:40 +0200

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/PerfectSquares.kt
@@ -0,0 +1,18 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Reference: https://leetcode.com/problems/perfect-squares
+class PerfectSquares {
+    fun numSquares(n: Int): Int {
+        val dp = IntArray(n + 1) { Int.MAX_VALUE }
+        dp[0] = 0
+        for (i in 1..n) {
+            var j = 1
+            while (j * j <= i) {
+                dp[i] = minOf(dp[i], dp[i - j * j] + 1)
+                j++
+            }
+        }
+        return dp[n]
+    }
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/SortCharactersByFrequency.kt
@@ -0,0 +1,16 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/sort-characters-by-frequency
+class SortCharactersByFrequency {
+    fun frequencySort(s: String): String {
+        return s.groupingBy { it }
+            .eachCount()
+            .entries
+            .sortedByDescending { it.value }
+            .joinToString(separator = "") { (char, freq) -> char.toString().repeat(freq) }
+    }
+}
+
+fun main() {
+    SortCharactersByFrequency().frequencySort("tree")
+}
