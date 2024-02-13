7cfe07f02373ad81f88d2b5cf5194386ab151c19
bodyangug <aseevbogdan5@gmail.com>
2024-02-13 13:40:24 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/FindFirstPalindromicStringInArray.kt
@@ -0,0 +1,8 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/?envType=daily-question&envId=2024-02-13
+class FindFirstPalindromicStringInArray {
+    fun firstPalindrome(words: Array<String>): String {
+        return words.runCatching { return this.first { it.reversed() == it } }.getOrDefault("")
+    }
+}
