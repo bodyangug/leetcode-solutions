d639559831cb5598dd29a1f85d2050a1c1d9bc8b
bodyangug <aseevbogdan5@gmail.com>
2024-02-15 17:43:31 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/FindPolygonWithLargestPerimeter.kt
@@ -0,0 +1,20 @@
+package com.pandus.leetcode.solutions.daily
+
+import java.util.*
+
+
+//Reference:https://leetcode.com/problems/find-polygon-with-the-largest-perimeter
+class FindPolygonWithLargestPerimeter {
+    fun largestPerimeter(nums: IntArray): Long {
+        Arrays.sort(nums)
+        var previousElementsSum: Long = 0
+        var ans: Long = -1
+        for (num in nums) {
+            if (num < previousElementsSum) {
+                ans = num + previousElementsSum
+            }
+            previousElementsSum += num.toLong()
+        }
+        return ans
+    }
+}
