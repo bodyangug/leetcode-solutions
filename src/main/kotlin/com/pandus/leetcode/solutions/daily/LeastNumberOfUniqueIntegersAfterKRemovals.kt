18f58119f5c237838c225d79e7dd0be781577870
bodyangug <aseevbogdan5@gmail.com>
2024-02-16 14:52:37 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/LeastNumberOfUniqueIntegersAfterKRemovals.kt
@@ -0,0 +1,19 @@
+package com.pandus.leetcode.solutions.daily
+
+import java.util.*
+
+
+//Reference: https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals
+class LeastNumberOfUniqueIntegersAfterKRemovals {
+    fun findLeastNumOfUniqueInts(arr: IntArray, k: Int): Int {
+        val frequencies = arr.groupBy { it }.values.map { it.size }.sorted()
+        var elementsRemoved = 0
+        frequencies.forEachIndexed { index, frequency ->
+            elementsRemoved += frequency
+            if (elementsRemoved > k) {
+                return frequencies.size - index
+            }
+        }
+        return 0
+    }
+}
