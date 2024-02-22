dc4fb8d3c5f105fa5bd79f38c4a62ccca9405ffd
bodyangug <aseevbogdan5@gmail.com>
2024-02-22 12:54:41 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/FindTheTownJudge.kt
@@ -0,0 +1,26 @@
+package com.pandus.leetcode.solutions.daily
+
+import javax.swing.text.html.HTML.Attribute.N
+
+
+//Reference: https://leetcode.com/problems/find-the-town-judge
+class FindTheTownJudge {
+    fun findJudge(n: Int, trust: Array<IntArray>): Int {
+        if (trust.size < n - 1) {
+            return -1;
+        }
+        val trustScores = IntArray(n + 1)
+
+        trust.forEach { relation ->
+            trustScores[relation[0]]--
+            trustScores[relation[1]]++
+        }
+
+        (1..n).forEach {
+            if (trustScores[it] == n - 1) {
+                return it
+            }
+        }
+        return -1
+    }
+}
