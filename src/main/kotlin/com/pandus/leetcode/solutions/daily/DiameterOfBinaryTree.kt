9e0023b67202b624677ca9c17d9e6dd0e98c3912
bodyangug <aseevbogdan5@gmail.com>
2024-02-27 12:38:35 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/DiameterOfBinaryTree.kt
@@ -0,0 +1,24 @@
+package com.pandus.leetcode.solutions.daily
+
+import kotlin.math.max
+
+//Reference: https://leetcode.com/problems/diameter-of-binary-tree
+class DiameterOfBinaryTree {
+
+    private var ans = 0
+    fun diameterOfBinaryTree(root: TreeNode?): Int {
+        dfs(root)
+        return ans
+    }
+
+    private fun dfs(node: TreeNode?): Int {
+        if (node == null) return 0
+
+        val left = dfs(node.left)
+        val right = dfs(node.right)
+
+        ans = max(ans, left + right)
+
+        return max(left, right) + 1
+    }
+}
