0111ec2f7d41a162cce57d41ef6a8b86d54a8e77
bodyangug <aseevbogdan5@gmail.com>
2024-02-28 12:46:40 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/FindBottomLeftTreeValue.kt
@@ -0,0 +1,24 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Reference: https://leetcode.com/problems/find-bottom-left-tree-value
+class FindBottomLeftTreeValue {
+    private var ans = 0
+    private var maxDepth = -1
+    fun findBottomLeftValue(root: TreeNode?): Int {
+        root?.let {
+            dfs(it, 0)
+        }
+        return ans
+    }
+
+    private fun dfs(root: TreeNode?, depth: Int) {
+        root?.let {
+            if (depth > maxDepth) {
+                maxDepth = depth.also { ans = root.`val` }
+            }
+            dfs(root.left, depth + 1)
+            dfs(root.right, depth + 1)
+        }
+    }
+}
