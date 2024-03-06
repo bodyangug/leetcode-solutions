2ef7985ace4b669ff74cd4bdf7511a7aa8cd4757
bodyangug <aseevbogdan5@gmail.com>
2024-03-06 11:13:09 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/LinkedListCycle.kt
@@ -0,0 +1,21 @@
+package com.pandus.leetcode.solutions.daily
+
+import com.pandus.leetcode.solutions.model.ListNode
+import java.util.HashSet
+
+
+//Reference: https://leetcode.com/problems/linked-list-cycle
+class LinkedListCycle {
+    fun hasCycle(head: ListNode?): Boolean {
+        val nodesSeen = HashSet<ListNode>()
+        var current = head
+        while (current != null) {
+            if (nodesSeen.contains(current)) {
+                return true
+            }
+            nodesSeen.add(current)
+            current = current.next
+        }
+        return false
+    }
+}
