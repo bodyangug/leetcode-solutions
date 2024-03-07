6b87f1ac04cbf929ad00b56f648433bcf172104e
bodyangug <aseevbogdan5@gmail.com>
2024-03-07 11:31:43 +0200

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MiddleOfLinkedList.kt
@@ -0,0 +1,18 @@
+package com.pandus.leetcode.solutions.daily
+
+import com.pandus.leetcode.solutions.model.ListNode
+
+
+//Reference: https://leetcode.com/problems/middle-of-the-linked-list
+class MiddleOfLinkedList {
+    fun middleNode(head: ListNode?): ListNode? {
+        val temp = arrayOfNulls<ListNode>(100)
+        var h = head
+        var idx = 0
+        while (h != null) {
+            temp[idx++] = h
+            h = h.next
+        }
+        return temp[idx / 2]
+    }
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/weekly/WinnerOfLinkedListGame.kt
@@ -0,0 +1,29 @@
+package com.pandus.leetcode.solutions.weekly
+
+import com.pandus.leetcode.solutions.model.ListNode
+
+
+//Reference: https://leetcode.com/problems/winner-of-the-linked-list-game
+class WinnerOfLinkedListGame {
+    fun gameResult(head: ListNode?): String {
+        var even = head
+        var evenPoints = 0
+        var oddPoints = 0
+
+        while (even != null) {
+            val odd = even.next ?: break
+            if (even.`val` > odd.`val`) {
+                evenPoints += 1
+            } else {
+                oddPoints += 1
+            }
+            even = odd.next
+        }
+
+        return when {
+            oddPoints > evenPoints -> "Odd"
+            oddPoints < evenPoints -> "Even"
+            else -> "Tie"
+        }
+    }
+}
