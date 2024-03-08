946431a429931aedc35be13c942f030308366ad0
bodyangug <aseevbogdan5@gmail.com>
2024-03-08 14:18:07 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/CountElementsWithMaximumFrequency.kt
@@ -0,0 +1,11 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/count-elements-with-maximum-frequency
+class CountElementsWithMaximumFrequency {
+    fun maxFrequencyElements(nums: IntArray): Int {
+        val freqMap = LinkedHashMap<Int, Int>().apply {
+            nums.forEach { this[it] = this.getOrDefault(it, 0) + 1 }
+        }
+        return freqMap.maxOf { it.value }.let { max -> freqMap.values.filter { it == max }.sum() }
+    }
+}
