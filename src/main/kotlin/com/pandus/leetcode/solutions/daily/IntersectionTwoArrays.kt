103a71b4bcaf9dd3ae46d3939edb81f616d4c06e
bodyangug <aseevbogdan5@gmail.com>
2024-03-10 22:08:53 +0200

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/IntersectionTwoArrays.kt
@@ -0,0 +1,22 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/intersection-of-two-arrays
+class IntersectionTwoArrays {
+    fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
+        val set1: MutableSet<Int> = HashSet()
+        for (n in nums1) {
+            set1.add(n)
+        }
+        val set2: MutableSet<Int> = HashSet()
+        for (n in nums2) {
+            set2.add(n)
+        }
+        set1.retainAll(set2)
+        val output = IntArray(set1.size)
+        var idx = 0
+        for (s in set1) {
+            output[idx++] = s
+        }
+        return output
+    }
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MinimumCommonValue.kt
@@ -0,0 +1,18 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/minimum-common-value
+class MinimumCommonValue {
+    fun getCommon(nums1: IntArray, nums2: IntArray): Int {
+        val set1: MutableSet<Int> = HashSet()
+
+        for (num in nums1) {
+            set1.add(num)
+        }
+        for (num in nums2) {
+            if (set1.contains(num)) {
+                return num
+            }
+        }
+        return -1
+    }
+}
