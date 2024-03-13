617ac381012dc279b4924812b7c6023122f71d84
bodyangug <aseevbogdan5@gmail.com>
2024-03-13 22:31:16 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/FindPivotInteger.kt
@@ -0,0 +1,19 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/find-the-pivot-integer
+class FindPivotInteger {
+    fun pivotInteger(n: Int): Int {
+        var sum = n * (n + 1) / 2
+        var leftSum = 0
+
+        for (i in 1 until n) {
+            leftSum += i
+            sum -= i
+
+            if (leftSum == sum) {
+                return i + 1
+            }
+        }
+        return -1
+    }
+}
