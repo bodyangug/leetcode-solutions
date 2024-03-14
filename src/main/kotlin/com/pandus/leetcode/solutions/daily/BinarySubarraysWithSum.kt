35108bb3782a8f914df8e3e14384a790606a2676
bodyangug <aseevbogdan5@gmail.com>
2024-03-14 12:51:49 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/BinarySubarraysWithSum.kt
@@ -0,0 +1,23 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Reference: https://leetcode.com/problems/binary-subarrays-with-sum
+class BinarySubarraysWithSum {
+    fun numSubarraysWithSum(nums: IntArray, goal: Int): Int {
+        var totalCount = 0
+        var currentSum = 0
+        val freq: MutableMap<Int, Int> = HashMap()
+        for (num in nums) {
+            currentSum += num
+            if (currentSum == goal) {
+                totalCount++
+            }
+            if (freq.containsKey(currentSum - goal)) {
+                totalCount += freq[currentSum - goal]!!
+            }
+            freq[currentSum] = freq.getOrDefault(currentSum, 0) + 1
+        }
+        return totalCount
+    }
+}
+
