ed62a9e1d80fadde4fd21e475d8e25252c4c5c01
Bohdan <bohdan_asieiev@epam.com>
2024-03-27 14:32:45 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/SubarrayProductLessThanK.kt
@@ -0,0 +1,22 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/subarray-product-less-than-k
+class SubarrayProductLessThanK {
+    fun numSubarrayProductLessThanK(nums: IntArray, k: Int): Int {
+        if (k <= 1) return 0
+        var totalCount = 0
+        var product = 1
+
+        var left = 0
+        var right = 0
+        while (right < nums.size) {
+            product *= nums[right]
+            while (product >= k) {
+                product /= nums[left++]
+            }
+            totalCount += right - left + 1
+            right++
+        }
+        return totalCount
+    }
+}
