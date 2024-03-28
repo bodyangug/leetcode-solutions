1a9b8bad3ba9524c2a8736562b4faa9d10b332eb
Bohdan <bohdan_asieiev@epam.com>
2024-03-28 12:44:36 +0200

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/LengthOfLongestSubarrayWithAtMostKFrequency.kt
@@ -0,0 +1,21 @@
+package com.pandus.leetcode.solutions.daily
+
+import kotlin.math.max
+
+//Reference:https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency
+class LengthOfLongestSubarrayWithAtMostKFrequency {
+    fun maxSubarrayLength(nums: IntArray, k: Int): Int {
+        var ans = 0
+        var start = -1
+        val frequency= LinkedHashMap<Int, Int>()
+        for (idx in nums.indices) {
+            frequency[nums[idx]] = frequency.getOrDefault(nums[idx], 0) + 1
+            while (frequency[nums[idx]]!! > k) {
+                start++
+                frequency[nums[start]] = frequency[nums[start]]!! - 1
+            }
+            ans = max(ans, (idx - start))
+        }
+        return ans
+    }
+}
