f548cc0c966184e046f88b3de11d9efdc991ebfd
Bohdan <bohdan_asieiev@epam.com>
2024-04-07 13:43:26 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MaximumNestingDepthOfParentheses.kt
@@ -0,0 +1,20 @@
+package com.pandus.leetcode.solutions.daily
+
+import kotlin.math.max
+
+//Reference: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+class MaximumNestingDepthOfParentheses {
+    fun maxDepth(s: String): Int {
+        var ans = 0
+        var openBrackets = 0
+        s.forEach {
+            if (it == '(') {
+                openBrackets++
+            } else if (it == ')') {
+                openBrackets--
+            }
+            ans = max(ans, openBrackets)
+        }
+        return ans
+    }
+}
