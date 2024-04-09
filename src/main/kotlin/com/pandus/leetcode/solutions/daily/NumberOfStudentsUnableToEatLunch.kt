c0546b85f1a6a7ff7b57a61e5b99cf5b653257ce
Bohdan <bohdan_asieiev@epam.com>
2024-04-09 15:51:41 +0300

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/NumberOfStudentsUnableToEatLunch.kt
@@ -0,0 +1,26 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch
+class NumberOfStudentsUnableToEatLunch {
+    fun countStudents(students: IntArray, sandwiches: IntArray): Int {
+        var countPreferSquare = 0
+        var countPreferCircle = 0
+        students.forEach {
+            if (it == 1) countPreferSquare++
+            else countPreferCircle++
+        }
+        sandwiches.forEach {
+            // No student wants the circle sandwich on top of the stack
+            if (it == 0 && countPreferCircle == 0) return countPreferSquare;
+            // No student wants the square sandwich on top of the stack
+            if (it == 1 && countPreferSquare == 0) return countPreferCircle;
+            if (it == 1) countPreferSquare--
+            else countPreferCircle--
+        }
+        return 0
+    }
+}
+
+fun main() {
+    NumberOfStudentsUnableToEatLunch().countStudents(intArrayOf(1, 1, 1, 0, 0, 1), intArrayOf(1, 0, 0, 0, 1, 1))
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/TimeNeededToBuyTickets.kt
@@ -0,0 +1,17 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/time-needed-to-buy-tickets
+class TimeNeededToBuyTickets {
+    fun timeRequiredToBuy(tickets: IntArray, k: Int): Int {
+        var ans = 0
+        while (true) {
+            for (index in tickets.indices) {
+                if (tickets[index] == 0) continue
+                tickets[index]--
+                ans++
+                if (tickets[k] == 0) return ans
+            }
+        }
+    }
+}
+
