6b41ba4daf419ead3f107feb18b77aa0a9ae5754
bodyangug <aseevbogdan5@gmail.com>
2024-04-15 00:58:33 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/SumOfLeftLeaves.kt
@@ -0,0 +1,25 @@
+package com.pandus.leetcode.solutions.daily
+
+import com.pandus.leetcode.solutions.model.TreeNode
+
+//Reference: https://leetcode.com/problems/sum-of-left-leaves
+class SumOfLeftLeaves {
+    private var ans = 0
+    fun sumOfLeftLeaves(root: TreeNode?): Int {
+        dfs(root, false)
+        return ans
+    }
+
+    private fun dfs(root: TreeNode?, isLeft: Boolean) {
+        root?.let {
+            val left = it.left
+            val right = it.right
+
+            if (isLeft && left == null && right == null) {
+                ans += it.`val`
+            }
+            dfs(left, true)
+            dfs(right, false)
+        }
+    }
+}
