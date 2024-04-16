949a683cc9354428a9113ddcacf05383f5d7e91e
Bohdan <bohdan_asieiev@epam.com>
2024-04-16 13:39:29 +0300

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/AddOneRowToTree.kt
@@ -0,0 +1,44 @@
+package com.pandus.leetcode.solutions.daily
+
+import com.pandus.leetcode.solutions.model.TreeNode
+
+//Reference: https://leetcode.com/problems/add-one-row-to-tree
+class AddOneRowToTree {
+    fun addOneRow(
+        t: TreeNode?,
+        v: Int,
+        d: Int
+    ): TreeNode? {
+        if (d == 1) {
+            val n = TreeNode(v)
+            n.left = t
+            return n
+        }
+        insert(numToInsert = v, node = t, curDepth = 1, targetDepth = d)
+        return t
+    }
+
+    private fun insert(
+        numToInsert: Int,
+        node: TreeNode?,
+        curDepth: Int,
+        targetDepth: Int
+    ) {
+        if (node == null) return
+        if (curDepth == targetDepth - 1) {
+            //insert left
+            node.left?.let {
+                val t = it
+                node.left = TreeNode(numToInsert).apply { left = t }
+            }
+            //insert right
+            node.right?.let {
+                val t = it
+                node.right = TreeNode(numToInsert).apply { right = t }
+            }
+        } else {
+            insert(numToInsert, node.left, curDepth + 1, targetDepth)
+            insert(numToInsert, node.right, curDepth + 1, targetDepth)
+        }
+    }
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/SumRootToLeafNumbers.kt
@@ -0,0 +1,27 @@
+package com.pandus.leetcode.solutions.daily
+
+import com.pandus.leetcode.solutions.model.TreeNode
+
+
+//Reference: https://leetcode.com/problems/sum-root-to-leaf-numbers
+class SumRootToLeafNumbers {
+
+    private var rootLeaf = 0
+
+    fun sumNumbers(root: TreeNode?): Int {
+        dfs(root, 0)
+        return rootLeaf
+    }
+
+    private fun dfs(root: TreeNode?, num: Int) {
+        var num = num
+        if (root != null) {
+            num = num * 10 + root.`val`
+            if (root.left == null && root.right == null) {
+                rootLeaf += num
+            }
+            dfs(root.left, num)
+            dfs(root.right, num)
+        }
+    }
+}
