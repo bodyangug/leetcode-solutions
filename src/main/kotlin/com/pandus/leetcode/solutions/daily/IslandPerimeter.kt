124bd12f6febe769a429e694ab1463807ba857bf
Bohdan <bohdan_asieiev@epam.com>
2024-04-19 15:04:45 +0300

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/IslandPerimeter.kt
@@ -0,0 +1,47 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/island-perimeter
+class IslandPerimeter {
+
+    fun islandPerimeter(grid: Array<IntArray>): Int {
+        var up: Int
+        var down: Int
+        var left: Int
+        var right: Int
+        var result = 0
+        val rows: Int = grid.size
+        val cols: Int = grid[0].size
+
+        for (r in 0 until rows) {
+            for (c in 0 until cols) {
+                if (grid[r][c] == 1) {
+                    up = if (r == 0) {
+                        0
+                    } else {
+                        grid[r - 1][c]
+                    }
+                    left = if (c == 0) {
+                        0
+                    } else {
+                        grid[r][c - 1]
+                    }
+                    down = if (r == rows - 1) {
+                        0
+                    } else {
+                        grid[r + 1][c]
+                    }
+                    right = if (c == cols - 1) {
+                        0
+                    } else {
+                        grid[r][c + 1]
+                    }
+                    result += 4 - (up + left + right + down)
+                }
+            }
+        }
+
+        return result
+    }
+
+
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/NumberOfIslands.kt
@@ -0,0 +1,51 @@
+package com.pandus.leetcode.solutions.daily
+
+import java.util.LinkedList
+
+//Reference: https://leetcode.com/problems/number-of-islands
+class NumberOfIslands {
+
+    private val directions = arrayOf(
+        intArrayOf(0, 1), // up
+        intArrayOf(0, -1), // down
+        intArrayOf(-1, 0), // left
+        intArrayOf(1, 0) //
+    )
+    private var m: Int = -1
+    private var n: Int = -1
+
+    fun numIslands(grid: Array<CharArray>): Int {
+        var ans = 0
+        m = grid.size
+        n = grid[0].size
+
+        for (r in 0 until m) {
+            for (c in 0 until n) {
+                if (grid[r][c] == '1') {
+                    ans++
+                    bfs(grid, r, c)
+                }
+            }
+        }
+        return ans
+    }
+
+    private fun bfs(grid: Array<CharArray>, r: Int, c: Int) {
+        val queue = LinkedList<IntArray>()
+        queue.offer(intArrayOf(r, c))
+        while (queue.isNotEmpty()) {
+            val curr = queue.poll()
+            for (dir in directions) {
+                val nr = curr[0] + dir[0]
+                val nc = curr[1] + dir[1]
+                if ((nr in 0..<m) &&
+                    (nc in 0..<n) &&
+                    grid[nr][nc] == '1'
+                ) {
+                    grid[nr][nc] = '0'
+                    queue.offer(intArrayOf(nr, nc))
+                }
+            }
+        }
+    }
+}
