bfcd4908019264779133d9b3fc78f34744dbb32c
Bohdan <bohdan_asieiev@epam.com>
2024-05-09 11:30:07 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MaximizeHappinessOfSelectedChildren.kt
@@ -0,0 +1,22 @@
+package com.pandus.leetcode.solutions.daily
+
+import java.util.PriorityQueue
+import kotlin.math.max
+
+//Reference: https://leetcode.com/problems/maximize-happiness-of-selected-children
+class MaximizeHappinessOfSelectedChildren {
+    fun maximumHappinessSum(happiness: IntArray, k: Int): Long {
+        val pq = PriorityQueue(Comparator.reverseOrder<Int>())
+
+        for (h in happiness) {
+            pq.add(h)
+        }
+
+        var totalHappinessSum: Long = 0
+
+        for ((turns, _) in (0 until k).withIndex()) {
+            totalHappinessSum += max((pq.poll() - turns), 0)
+        }
+        return totalHappinessSum
+    }
+}
