b759394f96bfa02c1d12e222827246bc42daae66
Bohdan <bohdan_asieiev@epam.com>
2024-05-16 18:01:46 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/EvaluateBooleanBinaryTree.kt
@@ -0,0 +1,24 @@
+package com.pandus.leetcode.solutions.daily
+
+import com.pandus.leetcode.solutions.model.TreeNode
+
+//Reference: https://leetcode.com/problems/evaluate-boolean-binary-tree
+class EvaluateBooleanBinaryTree {
+    fun evaluateTree(root: TreeNode): Boolean {
+        // Handles the case for leaf nodes.
+        if (root.left == null && root.right == null) {
+            return root.`val` != 0
+        }
+
+        // Store the evaluations for the left subtree and right subtree.
+        val evaluateLeftSubtree = evaluateTree(root.left!!)
+        val evaluateRightSubtree = evaluateTree(root.right!!)
+        val evaluateRoot = if (root.`val` == 2) {
+            evaluateLeftSubtree or evaluateRightSubtree
+        } else {
+            evaluateLeftSubtree and evaluateRightSubtree
+        }
+
+        return evaluateRoot
+    }
+}
