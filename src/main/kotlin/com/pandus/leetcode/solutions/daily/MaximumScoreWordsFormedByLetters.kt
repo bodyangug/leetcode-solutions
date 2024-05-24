1c078e1e9c1bf50ad800a73f9876e4b50a7d81b0
Bohdan <bohdan_asieiev@epam.com>
2024-05-24 08:51:44 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MaximumScoreWordsFormedByLetters.kt
@@ -0,0 +1,81 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Reference: https://leetcode.com/problems/maximum-score-words-formed-by-letters
+class MaximumScoreWordsFormedByLetters {
+    private var maxScore = 0
+    private lateinit var freq: IntArray
+
+    fun maxScoreWords(words: Array<String>, letters: CharArray, score: IntArray): Int {
+        // Count how many times each letter occurs
+        freq = IntArray(26)
+        for (c in letters) {
+            freq[c - 'a']++
+        }
+        maxScore = 0
+        val memo = mutableMapOf<Pair<Int, IntArray>, Int>()
+        check(words.size - 1, words, score, IntArray(26), 0, memo)
+        return maxScore
+    }
+
+    // Check if adding this word exceeds the frequency of any letter
+    private fun isValidWord(subsetLetters: IntArray): Boolean {
+        for (c in 0..25) {
+            if (freq[c] < subsetLetters[c]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun check(
+        w: Int,
+        words: Array<String>,
+        score: IntArray,
+        subsetLetters: IntArray,
+        totalScore: Int,
+        memo: MutableMap<Pair<Int, IntArray>, Int>
+    ) {
+        if (w == -1) {
+            // If all words have been iterated, check the score of this subset
+            maxScore = maxOf(maxScore, totalScore)
+            return
+        }
+
+        val key = w to subsetLetters.copyOf()
+        if (memo.containsKey(key)) {
+            maxScore = maxOf(maxScore, memo[key]!!)
+            return
+        }
+
+        // Not adding words[w] to the current subset
+        check(w - 1, words, score, subsetLetters, totalScore, memo)
+
+        // Adding words[w] to the current subset
+        val L = words[w].length
+        var valid = true
+        var newScore = totalScore
+        for (i in 0 until L) {
+            val c = words[w][i] - 'a'
+            subsetLetters[c]++
+            newScore += score[c]
+            if (subsetLetters[c] > freq[c]) {
+                valid = false
+            }
+        }
+
+        if (valid) {
+            // Consider the next word if this subset is still valid
+            check(w - 1, words, score, subsetLetters, newScore, memo)
+        }
+
+        // Rollback effects of this word
+        for (i in 0 until L) {
+            val c = words[w][i] - 'a'
+            subsetLetters[c]--
+        }
+
+        memo[key] = maxScore
+    }
+
+}
