048887d587e9f2868a8b48702c6e6571ebe0726f
bodyangug <aseevbogdan5@gmail.com>
2024-06-03 21:39:18 +0300

    feat: add solutions

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/AppendCharactersToStringToMakeSubsequence.kt
@@ -0,0 +1,16 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/append-characters-to-string-to-make-subsequence
+class AppendCharactersToStringToMakeSubsequence {
+    fun appendCharacters(s: String, t: String): Int {
+        var first = 0
+        var longestPrefix = 0
+        while (first < s.length && longestPrefix < t.length) {
+            if (s[first] == t[longestPrefix]) {
+                longestPrefix++
+            }
+            first++
+        }
+        return t.length - longestPrefix
+    }
+}
---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/ReverseString.kt
@@ -0,0 +1,8 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/reverse-string
+class ReverseString {
+    fun reverseString(s: CharArray): Unit {
+        s.reverse()
+    }
+}
