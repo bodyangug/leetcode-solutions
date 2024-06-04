6b9d9f9e8268d89b59bfdee1bfe74945192c13b6
Bohdan <bohdan_asieiev@epam.com>
2024-06-04 13:27:54 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/LongestPalindrome.kt
@@ -0,0 +1,25 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Reference: https://leetcode.com/problems/longest-palindrome
+class LongestPalindrome {
+    fun longestPalindrome(s: String): Int {
+        var oddFreqCharsCount = 0
+        val frequencyMap: MutableMap<Char, Int> = HashMap()
+
+        for (c in s.toCharArray()) {
+            frequencyMap[c] = frequencyMap.getOrDefault(c, 0) + 1
+
+            if ((frequencyMap[c]!! % 2) == 1) {
+                oddFreqCharsCount++
+            } else {
+                oddFreqCharsCount--
+            }
+        }
+        return if (oddFreqCharsCount > 0) {
+            s.length - oddFreqCharsCount + 1
+        } else {
+            s.length
+        }
+    }
+}
