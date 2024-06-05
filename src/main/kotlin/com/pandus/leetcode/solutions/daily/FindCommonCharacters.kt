772d62bad3e57414504eb8d6b227395dd93e51fb
Bohdan <bohdan_asieiev@epam.com>
2024-06-05 12:11:39 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/FindCommonCharacters.kt
@@ -0,0 +1,32 @@
+package com.pandus.leetcode.solutions.daily
+
+import java.util.Arrays
+import kotlin.math.min
+
+
+//Reference: https://leetcode.com/problems/find-common-characters
+class FindCommonCharacters {
+    fun commonChars(words: Array<String>): List<String> {
+        val commonCharacterCounts = IntArray(26) { Int.MAX_VALUE }
+
+        for (word in words) {
+            val currentCharacterCounts = IntArray(26)
+            for (ch in word) {
+                currentCharacterCounts[ch - 'a']++
+            }
+            for (i in 0 until 26) {
+                commonCharacterCounts[i] = minOf(commonCharacterCounts[i], currentCharacterCounts[i])
+            }
+        }
+
+        val result = mutableListOf<String>()
+        for (i in 0 until 26) {
+            repeat(commonCharacterCounts[i]) {
+                result.add(('a' + i).toString())
+            }
+        }
+
+        return result
+    }
+
+}
