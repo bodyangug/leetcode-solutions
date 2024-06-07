fdcf1bf1994a6aa0056bbb686b65903223878b43
bodyangug <aseevbogdan5@gmail.com>
2024-06-07 21:52:14 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/ReplaceWords.kt
@@ -0,0 +1,21 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Reference: https://leetcode.com/problems/replace-words
+class ReplaceWords {
+    fun replaceWords(dictionary: List<String>, sentence: String): String {
+        val dictSet = dictionary.toSet()
+        val wordArray = sentence.split(" ").map { shortestRoot(it, dictSet) }
+        return wordArray.joinToString(" ")
+    }
+
+    private fun shortestRoot(word: String, dictSet: Set<String>): String {
+        for (i in 1..word.length) {
+            val root = word.substring(0, i)
+            if (dictSet.contains(root)) {
+                return root
+            }
+        }
+        return word
+    }
+}
