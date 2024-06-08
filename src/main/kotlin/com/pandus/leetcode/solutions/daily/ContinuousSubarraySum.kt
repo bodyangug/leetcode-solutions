8ffe70533c7f0cd1f9436aee3292445d398f50c2
Bohdan <bohdan_asieiev@epam.com>
2024-06-08 05:40:43 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/ContinuousSubarraySum.kt
@@ -0,0 +1,23 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Reference: https://leetcode.com/problems/continuous-subarray-sum/
+class ContinuousSubarraySum {
+    fun checkSubarraySum(nums: IntArray, k: Int): Boolean {
+        var prefixMod = 0
+        val modSeen = HashMap<Int, Int>()
+        modSeen[0] = -1
+
+        for (i in nums.indices) {
+            prefixMod = (prefixMod + nums[i]) % k
+            if (modSeen.containsKey(prefixMod)) {
+                if (i - modSeen[prefixMod]!! > 1) {
+                    return true
+                }
+            } else {
+                modSeen[prefixMod] = i
+            }
+        }
+        return false
+    }
+}
