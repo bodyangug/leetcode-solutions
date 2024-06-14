d51902cbb5ea29b01d011bcd17a1ea6ed9d031fd
Bohdan <bohdan_asieiev@epam.com>
2024-06-14 14:47:28 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MinimumIncrementToMakeArrayUnique.kt
@@ -0,0 +1,23 @@
+package com.pandus.leetcode.solutions.daily
+
+
+//Reference: https://leetcode.com/problems/minimum-increment-to-make-array-unique/
+class MinimumIncrementToMakeArrayUnique {
+    fun minIncrementForUnique(nums: IntArray): Int {
+        var minIncrements = 0
+        nums.sort()
+
+        for (i in 1 until nums.size) {
+            // Ensure each element is greater than its previous
+            if (nums[i] <= nums[i - 1]) {
+                // Add the required increment to minIncrements
+                val increment = nums[i - 1] + 1 - nums[i]
+                minIncrements += increment
+
+                // Set the element to be greater than its previous
+                nums[i] = nums[i - 1] + 1
+            }
+        }
+        return minIncrements
+    }
+}
