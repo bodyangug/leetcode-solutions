670304962ec5718bb5ac33f8078e1624168b62f8
Bohdan <bohdan_asieiev@epam.com>
2024-07-03 12:05:17 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves.kt
@@ -0,0 +1,26 @@
+package com.pandus.leetcode.solutions.daily
+
+import kotlin.math.min
+
+
+//Reference: https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves
+class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves {
+    fun minDifference(nums: IntArray): Int {
+        val numsSize = nums.size
+        if (numsSize <= 4) return 0
+
+        nums.sort()
+
+        var minDiff = Int.MAX_VALUE
+
+        var left = 0
+        var right = numsSize - 4
+        while (left < 4) {
+            minDiff = min(minDiff, (nums[right] - nums[left]))
+            left++
+            right++
+        }
+
+        return minDiff
+    }
+}
