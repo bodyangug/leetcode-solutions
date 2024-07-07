c08d9ef884f1803947875ad69e34e048f1cab444
Bohdan <bohdan_asieiev@epam.com>
2024-07-07 12:39:02 +0300

    feat: add solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/WaterBottles.kt
@@ -0,0 +1,16 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/water-bottles
+class WaterBottles {
+    fun numWaterBottles(numBottles: Int, numExchange: Int): Int {
+        var numBottles = numBottles
+        var consumedBottles = 0
+
+        while (numBottles >= numExchange) {
+            consumedBottles += numExchange
+            numBottles -= numExchange
+            numBottles++
+        }
+        return consumedBottles + numBottles
+    }
+}
