8f2fcec3e7c8b112a14979136b4e55e1a9229c8d
Bohdan <bohdan_asieiev@epam.com>
2024-07-10 00:28:06 +0300

    feat: Add solution for AverageWaitingTime problem

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/AverageWaitingTime.kt
@@ -0,0 +1,18 @@
+package com.pandus.leetcode.solutions.daily
+
+import java.lang.Integer.max
+
+//Reference: https://leetcode.com/problems/average-waiting-time
+class AverageWaitingTime {
+    fun averageWaitingTime(customers: Array<IntArray>): Double {
+        var nextIdleTime = 0
+        var netWaitTime: Long = 0
+
+        for (i in customers.indices) {
+            nextIdleTime = max(customers[i][0], nextIdleTime) + customers[i][1]
+            netWaitTime += (nextIdleTime - customers[i][0])
+        }
+        val averageWaitTime = netWaitTime.toDouble() / customers.size
+        return averageWaitTime
+    }
+}
