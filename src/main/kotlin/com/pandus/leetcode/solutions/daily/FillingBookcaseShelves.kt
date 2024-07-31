9b8a6715d3b4539e5b08dbeb85a599bb3b131203
Bohdan <bohdan_asieiev@epam.com>
2024-07-31 15:21:07 +0300

    feat: add FillingBookcaseShelves solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/FillingBookcaseShelves.kt
@@ -0,0 +1,30 @@
+package com.pandus.leetcode.solutions.daily
+
+import java.lang.Integer.max
+import java.lang.Integer.min
+
+//Reference: https://leetcode.com/problems/filling-bookcase-shelves
+class FillingBookcaseShelves {
+    fun minHeightShelves(books: Array<IntArray>, shelfWidth: Int): Int {
+        val dp = IntArray(books.size + 1)
+
+        dp[0] = 0
+        dp[1] = books[0][1]
+
+        for (i in 2..books.size) {
+            var remainingShelfWidth = shelfWidth - books[i - 1][0]
+            var maxHeight = books[i - 1][1]
+            dp[i] = books[i - 1][1] + dp[i - 1]
+
+            var j = i - 1
+            while (j > 0 && remainingShelfWidth - books[j - 1][0] >= 0) {
+                maxHeight = max(maxHeight, books[j - 1][1])
+                remainingShelfWidth -= books[j - 1][0]
+                dp[i] = min(dp[i], maxHeight + dp[j - 1])
+                j--
+            }
+        }
+
+        return dp[books.size]
+    }
+}
