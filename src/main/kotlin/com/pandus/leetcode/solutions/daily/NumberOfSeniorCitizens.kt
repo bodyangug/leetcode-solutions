735e4f8c6cecef880f85ae99e039bb543981f280
Bohdan <bohdan_asieiev@epam.com>
2024-08-01 14:29:20 +0300

    feat: add NumberOfSeniorCitizens solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/NumberOfSeniorCitizens.kt
@@ -0,0 +1,8 @@
+package com.pandus.leetcode.solutions.daily
+
+//Reference: https://leetcode.com/problems/number-of-senior-citizens
+class NumberOfSeniorCitizens {
+    fun countSeniors(details: Array<String>): Int {
+        return details.count { it.substring(11, 13).toInt() > 60 }
+    }
+}
