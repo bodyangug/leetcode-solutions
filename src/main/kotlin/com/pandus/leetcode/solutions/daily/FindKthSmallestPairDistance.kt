f46bfa5e93e7995fea54fa76cd814040d513a05e
Bohdan <bohdan_asieiev@epam.com>
2024-08-14 19:45:38 +0300

    feat: add FindKthSmallestPairDistance solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/FindKthSmallestPairDistance.kt
@@ -0,0 +1,34 @@
+package com.pandus.leetcode.solutions.daily
+
+import kotlin.math.abs
+
+//Reference: https://leetcode.com/problems/find-k-th-smallest-pair-distance
+class FindKthSmallestPairDistance {
+    fun smallestDistancePair(nums: IntArray, k: Int): Int {
+        var k = k
+        val arrayLength = nums.size
+        val maxElement = nums.max()
+        val distanceBucket = IntArray(maxElement + 1)
+
+        // Populate the bucket array with counts of each distance
+        for (i in 0 until arrayLength) {
+            for (j in i + 1 until arrayLength) {
+                // Compute the distance between nums[i] and nums[j]
+                val distance = abs((nums[i] - nums[j]))
+                // Increment the count for this distance in the bucket
+                ++distanceBucket[distance]
+            }
+        }
+
+        // Find the k-th smallest distance
+        for (dist in 0..maxElement) {
+            // Reduce k by the number of pairs with the current distance
+            k -= distanceBucket[dist]
+            // If k is less than or equal to 0, return the current distance
+            if (k <= 0) {
+                return dist
+            }
+        }
+        return -1 // Return -1 if no distance found, should not reach here
+    }
+}
