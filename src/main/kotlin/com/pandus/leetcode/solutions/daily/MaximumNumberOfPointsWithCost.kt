11f1f56ae9f51886d52c8b2a92a390aeace583c1
Bohdan <bohdan_asieiev@epam.com>
2024-08-17 22:23:20 +0300

    feat: add MaximumNumberOfPointsWithCost solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MaximumNumberOfPointsWithCost.kt
@@ -0,0 +1,33 @@
+package com.pandus.leetcode.solutions.daily
+
+import kotlin.math.max
+
+//Reference: https://leetcode.com/problems/maximum-number-of-points-with-cost
+class MaximumNumberOfPointsWithCost {
+    fun maxPoints(points: Array<IntArray>): Long {
+        val n = points[0].size
+
+        var currentRowMaxPoints = LongArray(n)
+
+        val inf = 1L shl 60
+
+        for (row in points) {
+            val nextRowMaxPoints = LongArray(n)
+
+            var leftMax = -inf
+            var rightMax = -inf
+
+            for (j in 0..<n) {
+                leftMax = max(leftMax, currentRowMaxPoints[j] + j)
+                nextRowMaxPoints[j] = max(nextRowMaxPoints[j], row[j] + leftMax - j)
+            }
+            for (j in n - 1 downTo 0) {
+                rightMax = max(rightMax, currentRowMaxPoints[j] - j)
+                nextRowMaxPoints[j] = max(nextRowMaxPoints[j], row[j] + rightMax + j)
+            }
+
+            currentRowMaxPoints = nextRowMaxPoints
+        }
+        return currentRowMaxPoints.max()
+    }
+}
