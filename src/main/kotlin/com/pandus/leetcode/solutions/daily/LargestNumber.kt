6e01dc560d9e5ef0e15af4ffb159ca3da1687a87
bodyangug <aseevbogdan5@gmail.com>
2024-09-18 17:43:27 +0300

    fix: resolve detekt

--- src/main/kotlin/com/pandus/leetcode/solutions/daily/LargestNumber.kt
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/LargestNumber.kt
@@ -4,16 +4,16 @@ package com.pandus.leetcode.solutions.daily
 class LargestNumber {
     fun largestNumber(nums: IntArray): String {
         val sorted = nums.sortedWith { a, b ->
-            val a1 = a.toString();
-            val b1 = b.toString();
-            (b1 + a1).compareTo(a1 + b1);
+            val a1 = a.toString()
+            val b1 = b.toString()
+            (b1 + a1).compareTo(a1 + b1)
         }
-        if (sorted[0] == 0) return "0";
+        if (sorted[0] == 0) return "0"
 
-        val ans = StringBuilder();
+        val ans = StringBuilder()
         for (it in sorted) {
-            ans.append(it.toString());
+            ans.append(it.toString())
         }
-        return ans.toString();
+        return ans.toString()
     }
 }
