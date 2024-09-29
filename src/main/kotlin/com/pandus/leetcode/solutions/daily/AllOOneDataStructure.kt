81c57ac6087224880c1db0713af2fb32225427fb
bodyangug <aseevbogdan5@gmail.com>
2024-09-29 23:17:14 +0300

    feat: add AllOOneDataStructure solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/AllOOneDataStructure.kt
@@ -0,0 +1,47 @@
+package com.pandus.leetcode.solutions.daily
+
+// Reference: https://leetcode.com/problems/all-oone-data-structure
+class AllOOneDataStructure {
+    val hashmap = HashMap<String, Int>()
+
+    fun inc(key: String) {
+        if(hashmap.containsKey(key)){
+            hashmap[key] = hashmap.getValue(key) + 1
+        }else{
+            hashmap[key] = 1
+        }
+    }
+
+    fun dec(key: String) {
+        if(hashmap.containsKey(key)){
+            hashmap[key] = hashmap.getValue(key) - 1
+        }
+        if(hashmap[key] != null && hashmap.getValue(key) == 0){
+            hashmap.remove(key)
+        }
+    }
+
+    fun getMaxKey(): String {
+        var max = ""
+        var maxValue = 0
+        for(entry in hashmap){
+            if(entry.value > maxValue){
+                maxValue = entry.value
+                max = entry.key
+            }
+        }
+        return max
+    }
+
+    fun getMinKey(): String {
+        var min = ""
+        var minValue = Int.MAX_VALUE
+        for(entry in hashmap){
+            if(entry.value <= minValue){
+                minValue = entry.value
+                min = entry.key
+            }
+        }
+        return min
+    }
+}
