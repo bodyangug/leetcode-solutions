9c0ec16c80aae3653316538d63fcfb9552e1b992
bodyangug <aseevbogdan5@gmail.com>
2024-10-10 17:45:15 +0300

    fix: resolve detekt issue

--- src/main/kotlin/com/pandus/leetcode/solutions/daily/MaximumWidthRamp.kt
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MaximumWidthRamp.kt
@@ -7,7 +7,7 @@ import kotlin.math.min
 class MaximumWidthRamp {
     fun maxWidthRamp(nums: IntArray): Int {
         val inds = nums.indices.sortedBy { nums[it] }
-        var min = nums.size;
+        var min = nums.size
         var max = -1
         return inds.maxOf { i ->
             max = if (i < min) i else max(max, i)
