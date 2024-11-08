c1fd1b76ccb5d2c7f3149efa2cceea4ad48c0231
Bohdan <bohdan_asieiev@epam.com>
2024-11-08 02:28:55 +0200

    feat: add LargestCombinationWithBitwiseANDGreaterThanZero solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/LargestCombinationWithBitwiseANDGreaterThanZero.kt
@@ -0,0 +1,9 @@
+package com.pandus.leetcode.solutions.daily
+
+// Reference: https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero
+class LargestCombinationWithBitwiseANDGreaterThanZero {
+    fun largestCombination(candidates: IntArray) =
+        (0..31).maxOf { bit ->
+            candidates.sumBy { (it shr bit) and 1 }
+        }
+}
