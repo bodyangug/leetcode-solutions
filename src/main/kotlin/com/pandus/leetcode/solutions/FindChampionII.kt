6c4fd28ce1beb052f459e8acaa897534bd034530
bodyangug <aseevbogdan5@gmail.com>
2024-11-26 19:59:41 +0200

    feat: add FindChampionII solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/FindChampionII.kt
@@ -0,0 +1,8 @@
+package com.pandus.leetcode.solutions
+
+// Reference: https://leetcode.com/problems/find-champion-ii
+class FindChampionII {
+    fun findChampion(n: Int, edges: Array<IntArray>) =
+        ((0..<n) - edges.map { it[1] }.toSet())
+            .takeIf { it.size == 1 }?.first() ?: -1
+}
