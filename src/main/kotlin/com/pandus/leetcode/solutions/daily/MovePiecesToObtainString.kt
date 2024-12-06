56078612dad40310c0f31244277e91cf40f654c8
Bohdan <bohdan_asieiev@epam.com>
2024-12-06 02:01:23 +0200

    feat: add MovePiecesToObtainString solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MovePiecesToObtainString.kt
@@ -0,0 +1,18 @@
+package com.pandus.leetcode.solutions.daily
+
+// Reference: https://leetcode.com/problems/move-pieces-to-obtain-a-string
+class MovePiecesToObtainString {
+    fun canChange(start: String, target: String): Boolean {
+        var l = 0;
+        var r = 0
+        for ((i, s) in start.withIndex()) {
+            val t = target[i]
+            if (s == 'R') r++
+            if (t == 'L') l++
+            if (l * r > 0) return false
+            if (t == 'R' && --r < 0) return false
+            if (s == 'L' && --l < 0) return false
+        }
+        return l == 0 && r == 0
+    }
+}
