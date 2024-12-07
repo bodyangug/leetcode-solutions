4d9094e11a115f5e1bf7cdca996f8da057daeedf
bodyangug <aseevbogdan5@gmail.com>
2024-12-07 16:23:34 +0200

    chore: fix detekt

--- src/main/kotlin/com/pandus/leetcode/solutions/daily/MovePiecesToObtainString.kt
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MovePiecesToObtainString.kt
@@ -3,7 +3,7 @@ package com.pandus.leetcode.solutions.daily
 // Reference: https://leetcode.com/problems/move-pieces-to-obtain-a-string
 class MovePiecesToObtainString {
     fun canChange(start: String, target: String): Boolean {
-        var l = 0;
+        var l = 0
         var r = 0
         for ((i, s) in start.withIndex()) {
             val t = target[i]
