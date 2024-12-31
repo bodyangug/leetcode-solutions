ac71bd408e4d477832d7f150ae9168ed5121e608
Bohdan Asieiev <bohdan_asieiev@epam.com>
2024-12-31 02:43:07 +0200

    feat: add MinimumCostForTickets solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MinimumCostForTickets.kt
@@ -0,0 +1,29 @@
+package com.pandus.leetcode.solutions.daily
+
+import kotlin.math.max
+import kotlin.math.min
+
+
+// Reference: https://leetcode.com/problems/minimum-cost-for-tickets
+class MinimumCostForTickets {
+    fun mincostTickets(days: IntArray, costs: IntArray): Int {
+        val lastDay = days[days.size - 1]
+        val dp = IntArray(lastDay + 1) { 0 }
+        var i = 0
+        for (day in 1..lastDay) {
+            if (day < days[i]) {
+                dp[day] = dp[day - 1]
+            } else {
+                i++
+                dp[day] = min(
+                    (dp[day - 1] + costs[0]),
+                    min(
+                        (dp[max(0, (day - 7))] + costs[1]),
+                        (dp[max(0, (day - 30))] + costs[2])
+                    )
+                )
+            }
+        }
+        return dp[lastDay]
+    }
+}
