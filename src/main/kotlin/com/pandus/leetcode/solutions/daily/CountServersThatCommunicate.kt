d500c23259b520a8271f567021548ee5d2046122
bodyangug <aseevbogdan5@gmail.com>
2025-01-23 23:04:03 +0200

    feature: add CountServersThatCommunicate solution

--- src/main/kotlin/com/pandus/leetcode/solutions/daily/CountServersThatCommunicate.kt
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/CountServersThatCommunicate.kt
@@ -3,6 +3,8 @@ package com.pandus.leetcode.solutions.daily
 // Reference: https://leetcode.com/problems/count-servers-that-communicate
 class CountServersThatCommunicate {
     fun countServers(g: Array<IntArray>) = g
-        .flatMap { r -> r.indices.map { r to it }}
+        .flatMap { r ->
+            r.indices.map { r to it }
+        }
         .count { (r, x) -> r[x] * g.sumOf { it[x] } * r.sum() > 1 }
 }
