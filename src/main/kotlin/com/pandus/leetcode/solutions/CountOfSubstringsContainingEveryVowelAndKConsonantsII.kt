5add1a6090c2c2bba53d0c7cc8d4b1672e0dcbd2
Bohdan <aseevbogdan5@gmail.com>
2025-03-11 00:10:35 +0200

    feat: add CountOfSubstringsContainingEveryVowelAndKConsonantsII solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/CountOfSubstringsContainingEveryVowelAndKConsonantsII.kt
@@ -0,0 +1,24 @@
+package com.pandus.leetcode.solutions
+
+// Reference: https://leetcode.com/problems/count-of-substrings-containing-every-vowel-and-k-consonants-ii
+class CountOfSubstringsContainingEveryVowelAndKConsonantsII {
+    fun countOfSubstrings(w: String, k: Int): Long {
+        fun atLeast(k: Int): Long {
+            var r = 0L
+            var j = 0
+            val cnt = IntArray(6)
+            var u = 0
+            for (i in w.indices) {
+                val p = "aeiou".indexOf(w[i]) + 1
+                if (cnt[p]++ < 1 && p > 0) u++
+                while (u == 5 && cnt[0] >= k) {
+                    r += w.length - i
+                    val p = "aeiou".indexOf(w[j++]) + 1
+                    if (--cnt[p] < 1 && p > 0) u--
+                }
+            }
+            return r
+        }
+        return atLeast(k) - atLeast(k + 1)
+    }
+}
