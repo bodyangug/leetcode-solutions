505ebe1998a117f21c946884e16dfcf368ce8ad5
Bohdan <aseevbogdan5@gmail.com>
2025-04-18 09:40:46 +0300

    feat: add CountAndSay solution

---
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/CountAndSay.kt
@@ -0,0 +1,32 @@
+package com.pandus.leetcode.solutions.daily
+
+// Reference: https://leetcode.com/problems/count-and-say
+class CountAndSay {
+    fun countAndSay(n: Int): String {
+        if (n <= 0) return ""
+
+        var current = "1"
+
+        repeat(n - 1) {
+            current = runLengthEncode(current)
+        }
+        return current
+    }
+
+    fun runLengthEncode(s: String): String {
+        val sb = StringBuilder()
+        var count = 1
+
+        for (i in 1 until s.length) {
+            if (s[i] == s[i - 1]) {
+                count++
+            } else {
+                sb.append(count).append(s[i - 1])
+                count = 1
+            }
+        }
+        sb.append(count).append(s.last())
+        return sb.toString()
+    }
+
+}
