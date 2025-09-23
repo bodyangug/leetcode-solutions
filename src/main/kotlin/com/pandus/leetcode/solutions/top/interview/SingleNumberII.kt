efd87e6c2cf3b666229e6fd84ba0a4e58ccd92e2
Bohdan <aseevbogdan5@gmail.com>
2025-09-23 15:43:41 +0200

    chore: resolve detekt issues

--- src/main/kotlin/com/pandus/leetcode/solutions/top/interview/SingleNumberII.kt
+++ src/main/kotlin/com/pandus/leetcode/solutions/top/interview/SingleNumberII.kt
@@ -6,15 +6,15 @@ class SingleNumberII {
         var result = 0
         var count: Int
         var bitMask: Int
-        for (i in 0 until 32){
+        for (i in 0 until 32) {
             count = 0
             bitMask = 1 shl i
 
             nums.forEach {
-                if(it and bitMask != 0) count++
+                if (it and bitMask != 0) count++
             }
 
-            if (count%3>0) result = result or bitMask
+            if (count % 3 > 0) result = result or bitMask
         }
         return result
     }
