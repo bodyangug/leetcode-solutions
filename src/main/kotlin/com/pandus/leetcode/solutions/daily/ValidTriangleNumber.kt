de7e3bc884a0ae88036ef88a1e641f7136b2f7d7
Bohdan <aseevbogdan5@gmail.com>
2025-09-26 14:09:59 +0200

    feat: add ValidTriangleNumber a solution

--- src/main/kotlin/com/pandus/leetcode/solutions/daily/ValidTriangleNumber.kt
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/ValidTriangleNumber.kt
@@ -3,19 +3,22 @@ package com.pandus.leetcode.solutions.daily
 // Reference: https://leetcode.com/problems/valid-triangle-number
 class ValidTriangleNumber {
     fun triangleNumber(nums: IntArray): Int {
-        nums.sort()
+        nums.sortDescending()
         var count = 0
-        for (i in 0 until nums.size - 2) {
-            var left = i + 1
-            var right = nums.size - 1
+        for (maxSideIdx in 0 until nums.size - 2) {
+            var leftIdx = maxSideIdx + 1
+            var rightIdx = nums.size - 1
 
-            while (left < right) {
-                val sum = nums[i] + nums[left]
-                if (sum > nums[right]) {
-                    count += right - left
-                    left++
+            while (leftIdx < rightIdx) {
+                val maxSide = nums[maxSideIdx]
+                val leftSide = nums[leftIdx]
+                val rightSide = nums[rightIdx]
+
+                if (leftSide + rightSide > maxSide) {
+                    count += rightIdx - leftIdx
+                    leftIdx++
                 } else {
-                    right--
+                    rightIdx--
                 }
             }
         }
