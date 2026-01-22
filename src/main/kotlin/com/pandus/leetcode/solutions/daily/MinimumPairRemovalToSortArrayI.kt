f6309e7e38b78cffe66e5808dcb0c0b2455c0f58
Bohdan <aseevbogdan5@gmail.com>
2026-01-22 09:26:28 +0100

    refactor: clean up comments in MinimumPairRemovalToSortArrayI.kt

--- src/main/kotlin/com/pandus/leetcode/solutions/daily/MinimumPairRemovalToSortArrayI.kt
+++ src/main/kotlin/com/pandus/leetcode/solutions/daily/MinimumPairRemovalToSortArrayI.kt
@@ -1,13 +1,7 @@
 package com.pandus.leetcode.solutions.daily
 
-
 // Reference: https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i
 class MinimumPairRemovalToSortArrayI {
-    // Given an array nums, you can perform the following operation any number of times:
-    //Select the adjacent pair with the minimum sum in nums. If multiple such pairs exist, choose the leftmost one.
-    //Replace the pair with their sum.
-    //Return the minimum number of operations needed to make the array non-decreasing.
-    //An array is said to be non-decreasing if each element is greater than or equal to its previous element (if it exists).
     fun minimumPairRemoval(nums: IntArray): Int {
         val list: MutableList<Int> = ArrayList()
         for (num in nums) {
@@ -41,7 +35,6 @@ class MinimumPairRemovalToSortArrayI {
             list[targetIndex] = minSum
             list.removeAt(targetIndex + 1)
         }
-
         return count
     }
 }
