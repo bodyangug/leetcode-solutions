5bec81dce611a1b1497e7b872b057ff6b7bfe2e5
Bohdan <aseevbogdan5@gmail.com>
2026-08-07 21:37:30 +0200

    feat: add RotateImage solution for rotating a matrix 90 degrees clockwise

---
+++ src/main/java/com/pandus/leetcode/solutions/google/RotateImage.java
@@ -0,0 +1,28 @@
+package com.pandus.leetcode.solutions.google;
+
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+}
