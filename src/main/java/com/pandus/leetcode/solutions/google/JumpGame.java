f6466494910470696c5cb1aeca121e05d6a2dd2b
Bohdan <aseevbogdan5@gmail.com>
2026-08-07 21:36:30 +0200

    feat: add JumpGame solution for determining if one can jump to the last index

---
+++ src/main/java/com/pandus/leetcode/solutions/google/JumpGame.java
@@ -0,0 +1,14 @@
+package com.pandus.leetcode.solutions.google;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+}
